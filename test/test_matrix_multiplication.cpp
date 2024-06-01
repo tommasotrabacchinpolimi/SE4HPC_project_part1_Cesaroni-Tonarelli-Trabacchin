2612cb6dcbfd470cc61f39d063a742e2cdc96020
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-06-01 09:59:31 +0200

    added some tests

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -28,6 +28,55 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
+TEST(MatrixMultiplicationTest, TestMultiplySquareMatrices) {
+    std::vector<std::vector<int>> A = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    std::vector<std::vector<int>> B = {
+        {10, 11, 12},
+        {13, 14, 15},
+        {16, 17, 18}
+    };
+    std::vector<std::vector<int>> C(3, std::vector<int>(3, 0));
+
+    multiplyMatrices(A, B, C, 3, 3, 3);
+
+    std::vector<std::vector<int>> expected = {
+        {84, 90, 96},
+        {201, 216, 231},
+        {318, 342, 366}
+    };
+
+    ASSERT_EQ(C, expected) << "Square Matrix multiplication test failed! :(((()";
+
+}
+
+TEST(MatrixMultiplicationTest, TestMultiplyCorrectMatrices) {
+    std::vector<std::vector<int>> A = {
+        {1, 0, 2},
+        {0, 1, -1},
+        {2, 8, 1}
+    };
+    std::vector<std::vector<int>> B = {
+        {1, 0, 0},
+        {0, 1, -1},
+        {-20, 0, 1}
+    };
+    std::vector<std::vector<int>> C(3, std::vector<int>(3, 0));
+
+    multiplyMatrices(A, B, C, 3, 3, 3);
+
+    std::vector<std::vector<int>> expected = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 1}
+    };
+
+    ASSERT_EQ(C, expected) << "Correct Matrix multiplication test failed! :(((()";
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
