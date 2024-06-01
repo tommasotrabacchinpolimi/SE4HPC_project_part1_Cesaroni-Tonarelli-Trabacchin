31ca00ff3339abbf1e9a91c359edefca48c7b8c3
Melanie <melanie.tonarelli@mail.polimi.it>
2024-06-01 15:54:19 +0200

    Added comments for test in part1

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -3,13 +3,58 @@
 #include <vector>
 #include <gtest/gtest.h>
 
+
 // ######################### Source code of multiplyMatrices in src/matrix_mult
 
+/*
+ ***************************************************************************************
+ ***************************************************************************************
+ * The found errors are:
+ * Error 1: Element-wise multiplication of ones detected!
+ * Error 2: Matrix A contains the number 7!
+ * Error 3: Matrix A contains a negative number!
+ * Error 4: Matrix B contains the number 3!
+ * Error 5: Matrix B contains a negative number!
+ * Error 6: Result matrix contains a number bigger than 100!
+ * Error 7: Result matrix contains a number between 11 and 20!
+ * Error 8: Result matrix contains zero!
+ * Error 9: Result matrix contains the number 99!
+ * Error 10: A row in matrix A contains more than one '1'!
+ * Error 11: Every row in matrix B contains at least one '0'!
+ * Error 12: The number of rows in A is equal to the number of columns in B!
+ * Error 13: The first element of matrix A is equal to the first element of matrix B!
+ * Error 14: The result matrix C has an even number of rows!
+ * Error 15: A row in matrix A is filled entirely with 5s!
+ * Error 16: Matrix B contains the number 6!
+ * Error 17: Result matrix C contains the number 17!
+ * Error 18: Matrix A is a square matrix!
+ * Error 19: Every row in matrix A contains the number 8!
+ * Error 20: Number of columns in matrix A is odd!
+ ***************************************************************************************
+ ***************************************************************************************
+*/
 
 /*
-    6,12,14,20
+ ***************************************************************************************
+ ***************************************************************************************
+ * The tests were generated in a black box manner.
+ * ....
+ ***************************************************************************************
+ ***************************************************************************************
+ */
+
+/*
+ ***************************************************************************************
+ ***************************************************************************************
+ * The below three tests verifies if the properties of the matrix multiplication
+ * are satisfied.
+ * 1) Associative properties: (AB)C = A(BC)
+ * 2) Identity element: AI = IA where I is the identity matrix
+ * 3) Null element: A0 = 0 where 0 is the matrix of all zero elements
+ ***************************************************************************************
+ ***************************************************************************************
 */
-TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
+TEST(MatrixMultiplicationTest, TestAssociativePropertyMatrices) {
     std::vector<std::vector<int>> A = {
         {1, 2, 3},
         {4, 5, 6}
@@ -28,12 +73,11 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
         {139, 154}
     };
 
-    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
+    ASSERT_EQ(C, expected) << "Associativity Matrix multiplication test failed! :(((()";
 }
-/*
-    6,2,12,18,20
-*/
-TEST(MatrixMultiplicationTest, TestMultiplySquareMatrices) {
+
+
+TEST(MatrixMultiplicationTest, TestIdentityElementMatrices) {
     std::vector<std::vector<int>> A = {
         {1, 2, 3},
         {4, 5, 6},
@@ -54,14 +98,12 @@ TEST(MatrixMultiplicationTest, TestMultiplySquareMatrices) {
         {318, 342, 366}
     };
 
-    ASSERT_EQ(C, expected) << "Square Matrix multiplication test failed! :(((()";
+    ASSERT_EQ(C, expected) << "Identity Element Matrix multiplication test failed! :(((()";
 
 }
 
-/*
-1,5,8,3,7,11,12,13,18,20
-*/
-TEST(MatrixMultiplicationTest, TestMultiplyCorrectMatrices) {
+
+TEST(MatrixMultiplicationTest, TestNullElementMatrices) {
     std::vector<std::vector<int>> A = {
         {1, 0, 2},
         {0, 1, -1},
@@ -82,8 +124,17 @@ TEST(MatrixMultiplicationTest, TestMultiplyCorrectMatrices) {
         {0, 0, 1}
     };
 
-    ASSERT_EQ(C, expected) << "Correct Matrix multiplication test failed! :(((()";
+    ASSERT_EQ(C, expected) << "Null Element Matrix multiplication test failed! :(((()";
 }
+
+/*
+ ***************************************************************************************
+ ***************************************************************************************
+ * From the above three tests, the following errors has been found: ...
+ * Therefore, the next tests were run in order to find related errors.
+ ***************************************************************************************
+ ***************************************************************************************
+ */
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
